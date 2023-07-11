56d65fde5ab5b74cdb9c09aead119c568e603161
Ilango <rajagopalilango@gmail.com>
2023-07-11 19:33:44 +0530

    📦 NEW: Add hello world route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(unused)]
+
+use std::net::SocketAddr;
+
+use axum::{
+    response::{Html, IntoResponse},
+    routing::get,
+    Router,
+};
+
+#[tokio::main]
+async fn main() {
+    let routes_hello = Router::new().route("/hello", get(handler_hello));
+
+    // start server
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    println!("->> LISTENING ON {addr}\n");
+    axum::Server::bind(&addr)
+        .serve(routes_hello.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn handler_hello() -> impl IntoResponse {
+    println!("->> {:<12} - handler_hello", "HANDLER");
+
+    Html("Hello <strong>World!</strong>")
 }
