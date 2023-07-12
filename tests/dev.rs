280d924af1f76e41215402b17f4d3d7a3071fd95
Ilango <rajagopalilango@gmail.com>
2023-07-12 19:26:54 +0530

    👌 IMPROVE: Update tests

--- tests/dev.rs
+++ tests/dev.rs
@@ -6,8 +6,12 @@ use anyhow::Result;
 async fn dev() -> Result<()> {
     let hc = httpc_test::new_client("http://localhost:8080")?;
 
+    // hello world
     hc.do_get("/hello?name=Ilango").await?.print().await?;
     hc.do_get("/hello2/Ilango").await?.print().await?;
+
+    // static
+    // hc.do_get("/src/main.rs").await?.print().await?;
 
     Ok(())
 }
