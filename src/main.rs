e04f30745c03d5478cd7694b8bee3569e0722372
Ilango <rajagopalilango@gmail.com>
2023-07-12 19:53:26 +0530

    👌 IMPROVE: Use login route in main route

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,25 @@ use std::net::SocketAddr;
 
 use axum::{
     extract::{Path, Query},
-    response::{Html, IntoResponse},
+    response::{Html, IntoResponse, Response},
     routing::{get, get_service},
-    Router,
+    Router, middleware,
 };
 use serde::Deserialize;
 use tower_http::services::ServeDir;
 
+pub use self::error::{Error, Result};
+
+mod error;
+mod web;
+
 #[tokio::main]
 async fn main() {
-    let routes_all = Router::new().merge(routes_hello()).fallback_service(routes_static());
+    let routes_all = Router::new()
+        .merge(routes_hello())
+        .merge(web::routes_login::routes())
+        .layer(middleware::map_response(main_response_mapper))
+        .fallback_service(routes_static());
 
     // start server
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
@@ -22,6 +31,13 @@ async fn main() {
         .serve(routes_all.into_make_service())
         .await
         .unwrap();
+}
+
+async fn main_response_mapper(res: Response) -> Response {
+    println!("->> {:<12} - main_response_mapper", "RES_MAPPER");
+    println!();
+
+    res
 }
 
 fn routes_hello() -> Router {
