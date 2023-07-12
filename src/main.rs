06b9e23a75cc06ecdf726602f9d1994962e3c9bf
Ilango <rajagopalilango@gmail.com>
2023-07-12 16:25:57 +0530

    👌 IMPROVE: Query and path params for hello world routes and make routes composable

--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,46 @@
 use std::net::SocketAddr;
 
 use axum::{
+    extract::{Path, Query},
     response::{Html, IntoResponse},
     routing::get,
     Router,
 };
+use serde::Deserialize;
 
 #[tokio::main]
 async fn main() {
-    let routes_hello = Router::new().route("/hello", get(handler_hello));
+    let routes_all = Router::new().merge(routes_hello());
 
     // start server
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
     println!("->> LISTENING ON {addr}\n");
     axum::Server::bind(&addr)
-        .serve(routes_hello.into_make_service())
+        .serve(routes_all.into_make_service())
         .await
         .unwrap();
 }
 
-async fn handler_hello() -> impl IntoResponse {
-    println!("->> {:<12} - handler_hello", "HANDLER");
+fn routes_hello() -> Router {
+    Router::new()
+        .route("/hello", get(handler_hello))
+        .route("/hello2/:name", get(handler_hello2))
+}
 
-    Html("Hello <strong>World!</strong>")
+#[derive(Debug, Deserialize)]
+struct HelloParams {
+    name: Option<String>,
+}
+
+async fn handler_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
+    println!("->> {:<12} - handler_hello - {params:?}", "HANDLER");
+
+    let name = params.name.as_deref().unwrap_or("World!");
+    Html(format!("Hello <strong>{name}</strong>"))
+}
+
+async fn handler_hello2(Path(name): Path<String>) -> impl IntoResponse {
+    println!("->> {:<12} - handler_hello - {name:?}", "HANDLER");
+
+    Html(format!("Hello <strong>{name}</strong>"))
 }
